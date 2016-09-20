ccea2ce63b86053a107ecc0a2db4d95f82a46d37
Toma <zelic.toma@gmail.com>
2016-09-20 15:44:54 +0200

    -feature - init basic server made

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 /**
  * Created by toma on 20.09.16..
  */
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+
+server.connection({ port : 3000});
+
+server.start(function(err){
+
+    if(err){
+        throw err;
+    }
+    console.log(`Server running at : ${server.info.uri}`);
+});
