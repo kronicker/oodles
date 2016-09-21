6c3f7cdd8f262965ba110aeeacf572718dfcb998
Toma <zelic.toma@gmail.com>
2016-09-21 15:02:16 +0200

    Fixed migrations

--- db/migrations.js
+++ db/migrations.js
@@ -1,26 +1,30 @@
 var thinky = require('thinky')();
 var type = thinky.type;
 
-var Oodler = thinky.createModel("Oodler", {
+var oodlerSchema={
     id: type.string(),
     firstName: type.string().min(2),
     lastName: type.string().min(2),
     email: type.string().email(),
     office: type.string().min(1)
-});
+};
 
-var Thingy = thinky.createModel("Thingy", {
+var thingySchema={
     id: type.string(),
     name: type.string().min(1),
     price: type.number().min(0.01),
     unit: type.string(),
     pictureUrl: type.string()
-});
+};
+
+var Oodler = thinky.createModel("Oodler", oodlerSchema);
+
+var Thingy = thinky.createModel("Thingy", thingySchema);
 
 var Oodlet = thinky.createModel("Oodlet", {
     id: type.string(),
     date: type.date().default(r.now()),
-    oodler: type.object(),
-    thingies: [type.object()],
+    oodler: type.object().schema(oodlerSchema),
+    thingies: [type.object().schema(thingySchema)],
     total: type.number()
 });
