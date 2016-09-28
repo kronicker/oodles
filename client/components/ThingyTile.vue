8fc2370530c1fceecf6af3dc0f7b035ff1cab8e2
Toma <zelic.toma@gmail.com>
2016-09-28 12:43:27 +0200

    Component structure start

---
+++ client/components/ThingyTile.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="thingyTile">
+    <img :src=thingy.pictureUrl>
+    <span>{{ thingy.name | capitalize}}</span>
+    <input v-model.number="qty" type="number">
+    <button @click="addThingy">Add</button>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  export default{
+//    props:{thingy},
+    data(){
+      return {
+        qty : 1,
+        thingy : {
+          id: "9940988f-deaa-401d-a221-c4342282d754",
+          name: "Fish",
+          pictureUrl: "http://leda.org",
+          price: 575,
+          unit : "kg"
+        }
+      }
+    }
+  }
+</script>
