5a37315f31ed233d8020d0f67af7ee539fe410b4
Toma <zelic.toma@gmail.com>
2016-10-04 16:43:51 +0200

    Connect to API
    Added loading thingies from db

--- client/views/OodletView.vue
+++ client/views/OodletView.vue
@@ -58,86 +58,7 @@
     data(){
       return {
         searchString: "",
-        thingies: [
-          {
-            id: "9940988f-deaa-401d-a221-c4342282d754",
-            name: "Fish",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 575,
-            unit: "kg"
-          },
-
-          {
-            id: "4a69eead-d9fa-452a-ba7f-0b97f50d60ea",
-            name: "Keyboard",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 451,
-            unit: "kom"
-          },
-
-          {
-            id: "cc79927f-af69-4931-8fbd-48538781f1a9",
-            name: "Bacon",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 634,
-            unit: "kom"
-          },
-
-          {
-            id: "940114d0-740c-484e-830d-0b5dcf93428f",
-            name: "Tuna",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 565,
-            unit: "kg"
-          },
-
-          {
-            id: "4e28a37b-8828-4b8a-acab-4420cf4a14ec",
-            name: "Chips",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 198,
-            unit: "kom"
-          },
-
-          {
-            id: "89c2703a-b988-4ab5-ac09-4b97002c1165",
-            name: "Hat",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 564,
-            unit: "kg"
-          },
-
-          {
-            id: "fd32cfd5-5675-4674-8672-80dad5ddeeff",
-            name: "Sausages",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 668,
-            unit: "kg"
-          },
-
-          {
-            id: "e83adda7-7273-4909-8161-398df25bceb4",
-            name: "Pants",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 491,
-            unit: "kg"
-          },
-
-          {
-            id: "90d3eadd-c8a3-4618-b656-e80cb43574f5",
-            name: "Hat",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 990,
-            unit: "kom"
-          },
-          {
-            id: "ebb99da6-6936-4084-bc60-1047eb44a09e",
-            name: "Bike",
-            pictureUrl: "https://unsplash.it/40/40?random",
-            price: 488,
-            unit: "kg"
-          }
-        ]
+        thingies: []
       }
     },
 
@@ -162,6 +83,12 @@
       thingyFinderUpdate(query) {
         this.searchString = query;
       }
+    },
+
+    created(){
+      this.$http.get('/thingy').then((response) => {
+        this.thingies = response.body;
+      });
     },
 
     components: {
