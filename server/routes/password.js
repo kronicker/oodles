c50ffa4dc77a66d75b7f31adbd0750b64d6e5f30
Toma <zelic.toma@gmail.com>
2016-10-12 16:33:04 +0200

    Bcrypt password update

--- server/routes/password.js
+++ server/routes/password.js
@@ -3,6 +3,7 @@ const Oodler = require('../models/oodler');
 const Token = require('../models/token');
 const moment = require('moment');
 const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 const api_key = 'key-73db2b70c2c5fda574df5e2fd938504f';
 const domain = 'sandbox629530a6164643d28eb2f1767607d8db.mailgun.org';
 const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
@@ -17,10 +18,34 @@ function sendResetEmail (token, email) {
           '.\nIf you did not request this password change please feel free to ignore it.' +
           '\nOodles'
   };
-  console.log(email);
+
   mailgun.messages().send(data, function (error, body) {
     console.log(body);
   });
+}
+
+function getToken(tokenId) {
+  return Token
+    .get(tokenId)
+    .run();
+}
+
+function createToken(userId) {
+  return Token({
+      value: crypto.randomBytes(64).toString('hex'),
+      expiresAt: moment().add(2, 'hours').toDate(),
+      userId: userId
+    })
+    .save();
+}
+
+function saveOodlerPassword(userId, hash) {
+  return Oodler
+    .get(request.payload.token.userId)
+    .update({
+      password: hash
+    })
+    .run();
 }
 
 function generateToken(request, reply) {
@@ -30,12 +55,7 @@ function generateToken(request, reply) {
     .filter({'email': email})
     .run()
     .then(result => {
-      return Token ({
-        value: crypto.randomBytes(64).toString('hex'),
-        expiresAt: moment().add(2, 'hours').toDate(),
-        userId: result[0].id
-        })
-        .save()
+      createToken(result[0].id)
         .then(result => {
           sendResetEmail(result, email);
           reply().code(200);
@@ -44,7 +64,23 @@ function generateToken(request, reply) {
 }
 
 function update(request, reply) {
-  
+  getToken(request.payload.token.id)
+    .then(result => {
+      if(moment(result.expiresAt).isBefore()) {
+        reply('Token expired').code(400);
+      }
+
+      else if(request.payload.password !== request.payload.passwordRepeat) {
+        reply('Passwords do not match').code(400);
+      }
+
+      bcrypt.hash(request.payload.password, function(err, hash) {
+        saveOodlerPassword(request.payload.token.userId, hash)
+          .then(result => {
+            reply(result).code(200);
+          });
+      });
+    });
 }
 
 let routes = [
