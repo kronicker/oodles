7db933027d85aa3f51bd9f50004b21b5d3dd3fb6
Toma <zelic.toma@gmail.com>
2016-10-12 09:12:55 +0200

    Router-link-active

--- client/components/NavBar.vue
+++ client/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="navBar">
     <ul class="nav nav-tabs nav-stacked" data-spy="affix" offset-top="300">
-      <li class=""><router-link to="/" data-toggle="tab"><span class="glyphicon glyphicon-shopping-cart"></span></router-link></li>
-      <li class=""><router-link to="/historyView" data-toggle="tab"><span class="glyphicon glyphicon-time"></span></router-link></li>
-      <li class=""><router-link to="/settingsView" data-toggle="tab"><span class="glyphicon glyphicon-cog"></span></router-link></li>
+      <li><router-link to="/" exact data-toggle="tab"><span class="glyphicon glyphicon-shopping-cart"></span></router-link></li>
+      <li><router-link to="/historyView" data-toggle="tab"><span class="glyphicon glyphicon-time"></span></router-link></li>
+      <li><router-link to="/settingsView" data-toggle="tab"><span class="glyphicon glyphicon-cog"></span></router-link></li>
     </ul>
   </div>
 </template>
@@ -19,6 +19,11 @@
     ul li a span { font-size: xx-large; }
 
     ul li a { text-align: center; }
+
+    .router-link-active{
+      background-color: #5bc0de;
+      color: #2b3e50;
+    }
   }
 
 </style>
