1d4d54f4b6ec59df452e9f8e7ab37f7fcfe58adc
Toma <zelic.toma@gmail.com>
2016-10-26 14:50:55 +0200

    Refactored reporters

--- server/monitor/index.js
+++ server/monitor/index.js
@@ -2,79 +2,29 @@
  * Created by toma on 25.10.16..
  */
 const good = require('good');
-const object = require('lodash');
-
 const MONITOR_PAYLOAD = process.env.MONITOR_PAYLOAD || false;
 
-let payloadReporter =  [
-  {
-    module: 'good-squeeze',
-    name: 'Squeeze',
-    args: [{request: '*', response: '*'}]
-  },
-  {
-    module: 'white-out',
-    args: [{
-      password: 'remove',
-      passwordRepeat: 'remove',
-      event: 'remove',
-      instance: 'remove',
-      labels: 'remove',
-      method: 'remove',
-      path: 'remove',
-      query: 'remove',
-      responseTime: 'remove',
-      statusCode: 'remove',
-      pid: 'remove',
-      httpVersion: 'remove',
-      source: 'remove',
-      route: 'remove',
-      log: 'remove',
-      config: 'remove',
-    }]
-  },
-  {
-    module: 'good-squeeze',
-    name: 'SafeJson',
-  },
-  {
-    module: 'good-squeeze-pretty',
-    args: [2]
-  },
-  'stdout'
-];
-let consoleReporter= [
-  {
-    module: 'good-squeeze',
-    name: 'Squeeze',
-    args: [{ log: '*', response: '*' }]
-  },
-  {
-    module: 'good-console'
-  },
-  'stdout'
-];
+const consoleReporter = require('./consoleReporter');
+const payloadReporter = require('./payloadReporter');
 
-let config = {
-  includes: {
-    request: ['payload'],
-    response: ['payload']
-  }
-};
+function reporters() {
+  let reporters = { consoleReporter: consoleReporter };
+
+  if(MONITOR_PAYLOAD) { reporters.payloadReporter = payloadReporter; }
+
+  return reporters;
+}
 
 let monitor = {
   register: (server, options, next) => {
     server.register({
       register: good,
-      options: () => {
-        if(MONITOR_PAYLOAD) {
-          config = object.assign(config, {reporters: { consoleReporter, payloadReporter }});
-          return config;
-        }
-        else {
-          config = object.assign(config, {reporters: { consoleReporter }});
-          return config;
-        }
+      options: {
+        includes: {
+          request: ['payload'],
+          response: ['payload']
+        },
+        reporters: reporters()
       },
     }, err => {
       if (err) {
