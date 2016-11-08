818d431f55e380c55589dba2a6ddc801938f95c5
Toma <zelic.toma@gmail.com>
2016-11-08 11:39:38 +0100

    Db models

--- server/models/totalOodlet.js
+++ server/models/totalOodlet.js
@@ -1,31 +1,27 @@
 /**
- * Created by toma on 21.09.16..
+ * Created by toma on 08.11.16..
  */
-'use strict';
-
 const object = require('lodash/object');
 const thinky = require('../db/thinky');
 const Thingy = require('./thingy');
 const Oodler = require('./oodler');
 const type = thinky.type;
-const dbConfig = require('../config').database;
 
 const schema = {
   id: type.string(),
   createdAt: type.date().default(() => new Date()),
   updatedAt: type.date().default(() => new Date()),
-  dueDate: type.date().default(() => dbConfig.defaultOodletDueDate),
-  orderedAt: type.date(),
+  orederedAt: type.date(),
   oodler: type.object().schema(Oodler.schema),
-  quantifiedThingies: [type.object().schema(object.merge(Thingy.schema, { qty: type.number() }))]
+  quantifiedThingies: [type.object().schema(object.merge(Thingy.schema, { qty: type.number() }))],
+  offices: type.array().schema(type.string())
 };
 
 module.exports = (() => {
-  var model = thinky.createModel("Oodlet", schema);
-
-  model.ensureIndex('dueDate');
-  model.ensureIndex('orderedAt');
+  var model = thinky.createModel("totalOodlet", schema);
+  
+  model.ensureIndex('orederedAt');
   model.schema = schema;
-
+  
   return model;
 })();
