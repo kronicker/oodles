b7de5dc179abe2822e59d4208f5203a23cb9474c
Toma <zelic.toma@gmail.com>
2016-12-06 14:29:57 +0100

    Settings view stylized

--- client/views/common/SettingsView.vue
+++ client/views/common/SettingsView.vue
@@ -4,12 +4,17 @@
       <h1 class="text-info">Settings</h1>
     </div>
     <div class="row">
-      <div class="col-md-4 col-md-offset-3 well">
-        <ul>
-          <li><span class="glyphicon glyphicon-user"></span><span><strong>  {{ oodler.firstName }} {{ oodler.lastName }}</strong></span></li>
-          <li><span class="glyphicon glyphicon-envelope"></span><span>  {{ oodler.email }}</span></li>
-          <li><span class="glyphicon glyphicon-home"></span><span>  Office {{ oodler.office }}</span></li>
-        </ul>
+      <div class="col-md-4 col-md-offset-3 jumbotron">
+        <h2>{{ oodler.firstName }} {{ oodler.lastName }}</h2>
+        <p><strong>Email: </strong> {{ oodler.email }} </p>
+        <p v-if="oodler.scope != 'admin'"><strong>Office: </strong> {{ oodler.office }} </p>
+        <div>
+          <a @click="resetPassword" class="btn btn-danger">Reset password</a>
+        </div>
+      </div>
+      <div id="message" v-show="message" class="col-md-4 col-md-offset-3 alert alert-dismissible">
+        <button @click="closeMessage" type="button" class="close">&times;</button>
+        {{ message }}
       </div>
     </div>
   </div>
@@ -17,9 +22,37 @@
 
 <script>
   export default {
+    data() {
+      return {
+        message : ''
+      }
+    },
+    
     computed: {
       oodler() {
         return this.$store.getters.oodler;
+      }
+    },
+    
+    methods: {
+      resetPassword() {
+        this.$http.post('/password/reset', {
+            email: this.oodler.email
+          })
+          .then(
+            response => {
+              this.message = 'Please check your email for password reset link!';
+              document.getElementById('message').classList.add('alert-info');
+              document.getElementById('message').classList.remove('alert-danger');
+            },
+            response => {
+              this.message = 'Oops! Something went wrong! Please try again!';
+              document.getElementById('message').classList.add('alert-danger');
+              document.getElementById('message').classList.remove('alert-info');
+            });
+      },
+      closeMessage() {
+        this.message = '';
       }
     }
   }
@@ -29,15 +62,8 @@
   #settingsView {
     .page-header { margin: 0px 0 10px; }
 
-    .well {
+    .jumbotron {
       margin-top: 200px;
-      ul {
-        list-style: none;
-        padding: 0;
-        font-size: x-large;
-
-        li { text-align: center; }
-      }
     }
 
   }
